3eb0ec36c8c230fa6ee693c826b7cba1d8617460
Isaac McGaha Miller <isaacmcgahamiller@gmail.com>
2017-09-15 19:49:04 -0600

    updated styling on user profile

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -27,56 +27,67 @@ class UserProfile extends Component {
 
     return (
       <div>
-        <div className="container-fluid">
-          <div className="profile-pets-container row">
-            <div className="col-sm-4">
-              <div className="profile-pic col-sm-4" style={{backgroundImage: `url(${user.profile_image.image_url})`}}></div>
-              <h3 className="profile-name">{user.name}</h3>
-            </div>
-            <div className="col-sm-8 container-fluid">
-              {isOwnProfile ? <h4>Your Pets</h4> : <h4>Pets {user.name} Owns</h4>}
-              {user.pets_owned.map(pet => (
-                <div key={pet.id} className="col-sm-3 profile-pet-row">
-                  <div className="profile-pet-row-item" alt="row of pets" style={{backgroundImage: `url(${pet.profile_image.image_url})`}}></div>
-                  <p className="pet-name">
-                    {pet.name}
-                    <span className="pet-followers">{pet.total_followers} <i className="fa fa-heart"></i></span>
-                  </p>
-                </div>
-              ))}
-            </div>
-            <div className="col-sm-6 container-fluid pet-followed-row">
-              {isOwnProfile ? <h4>Pets You Follow</h4> : <h4>Pets {user.name} Follows</h4>}
-              {user.pets_followed.map(pet => (
-                <div key={pet.id} className="col-sm-3 profile-pet-row">
-                  <div className="profile-pet-row-item" alt="row of pets" style={{backgroundImage: `url(${pet.profile_image.image_url})`}}></div>
-                  <p>{pet.name}</p>
-                </div>
-              ))}
-            </div>
-          </div>
-        </div>
-        <div className="col-sm-6">
-          {isOwnProfile ? <h4>Your Friends</h4> : <h4>Friends of {user.name}</h4>}
-          {user.friends.map(friend => (
-            <div key={friend.id} className="col-sm-3 profile-friend-row">
-              <Link to={`/profile/${friend.id}`}>
-                <div className="profile-friend-row-item" alt="row of friends" style={{backgroundImage: `url(${friend.profile_image.image_url})`}}></div>
-                <p>{friend.name}</p>
-              </Link>
-            </div>
-          ))}
+        <h2 className="col-sm-2">{user.name}</h2>
+        <div className="col-sm-8">
+          <div className="profile-pic" style={{backgroundImage: `url(${user.profile_image.image_url})`}} alt="Profile picture" />
         </div>
         {isOwnProfile &&
-          <div>
+          <div className="col-sm-2">
             <button className="btn btn-success profile-button"><h4>Add Pet</h4></button>
           </div>
         }
         {!isOwnProfile && !friendsWithUser &&
-          <div>
+          <div className="col-sm-2">
             <button className="btn btn-success profile-button"><h4>Send friend request</h4></button>
           </div>
         }
+        <div className="col-sm-12">
+          {isOwnProfile ? <h4>Your Pets</h4> : <h4>Pets {user.name} Owns</h4>}
+          <div className="profile-pet-row">
+            {user.pets_owned.map(pet => (
+              <div key={pet.id} className="profile-pet-container">
+                <div className="profile-pet-row-item" alt="row of pets" style={{backgroundImage: `url(${pet.profile_image.image_url})`}}></div>
+                <div className="pet-name">
+                  <p>
+                    {pet.name}
+                    <span className="pet-followers pull-right">{pet.total_followers} <i className="fa fa-heart"></i></span>
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="col-sm-12">
+          {isOwnProfile ? <h4>Pets You Follow</h4> : <h4>Pets {user.name} Follows</h4>}
+          <div className="profile-pet-row">
+            {user.pets_followed.map(pet => (
+              <div key={pet.id} className="profile-pet-container">
+                <div className="profile-pet-row-item" alt="row of pets" style={{backgroundImage: `url(${pet.profile_image.image_url})`}}></div>
+                <div className="pet-name">
+                  <p>
+                    {pet.name}
+                    <span className="pet-followers pull-right">{pet.total_followers} <i className="fa fa-heart"></i></span>
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="col-sm-12">
+          {isOwnProfile ? <h4>Your Friends</h4> : <h4>Friends of {user.name}</h4>}
+          <div className="profile-user-row">
+            {user.friends.map(friend => (
+              <div key={friend.id} className="profile-user-container">
+                <Link to={`/profile/${friend.id}`}>
+                  <div className="profile-user-row-item" alt="row of users" style={{backgroundImage: `url(${friend.profile_image.image_url})`}}></div>
+                  <div className="user-name">
+                    <p>{friend.name}</p>
+                  </div>
+                </Link>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     )
 
